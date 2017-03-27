fd436e2f2fd4e1c25dd00ae215ef3a3822bfc742
stephan <stephan.zerhusen@gmail.com>
2017-03-27 22:29:11 +0200

    prepared package.json and wp configs

---
+++ config/metadata.js
@@ -0,0 +1,8 @@
+const METADATA = {
+    title: 'react-typescript-bootstrap-webpack-starter',
+    description: 'A basic starter for a project build with ReactJS, Typescript, Webpack and Bootstrap',
+    host: 'localhost',
+    port: 3000
+};
+
+module.exports = METADATA;
