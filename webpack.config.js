94ef696d2f45061f50aa7f6ede818de93e6eff49
Stephan Zerhusen <s.zerhusen@byte-connect.de>
2017-03-28 18:04:11 +0200

    small improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 function buildConfig(env) {
-    return require('./config/webpack.' + env + '.config.js')({env: env})
+    return require('./config/webpack.' + env + '.config.js')(env)
 }
 
 module.exports = buildConfig;
