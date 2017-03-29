7d9f85c41acadf29fc60605ba7d399ca887d2545
stephan <stephan.zerhusen@gmail.com>
2017-03-29 23:08:21 +0200

    fixed props in Hello.tsx

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -6,10 +6,16 @@ export interface HelloProps {
 }
 
 export class Hello extends React.Component<HelloProps, any> {
+
+   // this contructor is necessary to make the props work
+   constructor(props: HelloProps, context: any) {
+      super(props, context);
+   }
+
    render() {
       return <div className="container">
          <div className="jumbotron">
-            <h1>Hello from <span className="myCustomStyleClass">Bla</span> and Blupp!</h1>
+            <h1>Hello from <span className="myCustomStyleClass">{this.props.compiler}</span> and {this.props.framework}!</h1>
             <p>Here is a fantastic Glyphicon <span className="glyphicon glyphicon-ok"></span></p>
          </div>
       </div>;
