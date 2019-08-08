08999eb144c00ac97142e9511f7af5393a7628d2
Gabriel Cardona <gabriel@bitcoin.com>
2019-08-07 22:41:57 -0700

    Start adding gui components.

--- config/metadata.js
+++ config/metadata.js
@@ -1,8 +1,8 @@
 const METADATA = {
-    title: 'react-typescript-bootstrap-webpack-starter',
-    description: 'A basic starter for a project build with ReactJS, Typescript, Webpack 2 and Bootstrap',
-    host: 'localhost',
-    port: 3000
-};
+  title: "bip70-merchant-services",
+  description: "Web GUI for bip 70 merchant services",
+  host: "localhost",
+  port: 3000
+}
 
-module.exports = METADATA;
+module.exports = METADATA
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,23 +1,38 @@
-import * as React from "react";
+import * as React from "react"
 
 export interface HelloProps {
-   compiler: string;
-   framework: string;
+  compiler: string
+  framework: string
 }
 
 export class Hello extends React.Component<HelloProps, any> {
+  // this constructor is necessary to make the props work
+  constructor(props: HelloProps, context: any) {
+    super(props, context)
+  }
 
-   // this constructor is necessary to make the props work
-   constructor(props: HelloProps, context: any) {
-      super(props, context);
-   }
-
-   render() {
-      return <div className="container">
-         <div className="jumbotron">
-            <h1>Hello from <span className="myCustomStyleClass">{this.props.compiler}</span> and {this.props.framework}!</h1>
-            <p>Here is a fantastic Glyphicon <span className="glyphicon glyphicon-ok"></span></p>
-         </div>
-      </div>;
-   }
+  render() {
+    return (
+      <div className="container">
+        <h1>Wikimedia Foundation</h1>
+        <h2>Bitcoin Donation</h2>
+        <p>Need Help?</p>
+        <div className="jumbotron">
+          <h1>
+            Hello from{" "}
+            <span className="myCustomStyleClass">{this.props.compiler}</span>{" "}
+            and {this.props.framework}!
+          </h1>
+          <p>
+            Here is a fantastic Glyphicon{" "}
+            <span className="glyphicon glyphicon-ok" />
+          </p>
+        </div>
+        <h2>
+          <button>Badger Button</button>
+        </h2>
+        <h2>Powered by bitcoin.com</h2>
+      </div>
+    )
+  }
 }
